941c3bccf246a5705b3ee952e27413d2df817929
gian <michaelgiantiqui3@gmail.com>
2023-07-30 21:08:04 +0800

    start

---
+++ Library Management System/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    class Book
+    {
+        private string name, content;
+        private int lastPage;
+
+        public Book(string name, string content)
+        {
+            this.name = name;
+            this.content = content;
+            this.lastPage = 0;
+        }
+
+
+    }
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
